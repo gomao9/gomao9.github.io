e5348bcc985a6c4a81ed137fd599a5339bbeb8c4
Sugita Haruhi <sugita@wyrd.co.jp>
2016-02-16 16:52:36 +0900

    検索機能を実装

--- static/memories/app.js
+++ static/memories/app.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
-  $.get("http://million-memories-server.herokuapp.com/search", {'speakers':'宮尾美也'});
-  //, function(data){
-  //  #alert("Data Loaded: ");
-  //#});
+
+  var speaker1 = "";
+  $("a.speaker1").click(function(){
+    speaker1 = $(this).text();
+  });
+
+  $("#search").click(function(){
+    $dialogs = $('#dialogs');
+    $.ajax({
+      type: "GET",
+      url: "http://million-memories-server.herokuapp.com/search",
+      data: {"speakers":speaker1},
+      dataType: "json",
+      success: function(data){
+        tags = data.map(function(dialog){
+          content = dialog.attributes.dialogs.map(function(d){
+            return $('<p></p>', {text:d.speaker + ':' + d.text});
+          });
+          return $('<li></li>', {class:"list-group-item"}).append($("<a></a>", {href:dialog.attributes.url, text:"参照"})).append(content);
+        });
+        return $dialogs.append(tags);
+      }
+    });
+  });
 });
