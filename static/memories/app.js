59bd6ead8b8ef9570c44b10b3cdf60cf24ca07b1
Sugita Haruhi <sugita@wyrd.co.jp>
2016-02-16 11:06:53 +0900

    memoriesを追加

---
+++ static/memories/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $.get("http://million-memories-server.herokuapp.com/search", {'speakers':'宮尾美也'});
+  //, function(data){
+  //  #alert("Data Loaded: ");
+  //#});
+});
