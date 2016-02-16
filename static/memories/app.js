4bad136d83bec17ac9218965f3b5edd5213eb2b2
gomao9 <gomaoq@gmail.com>
2016-02-16 21:52:34 +0900

    アイドル名をAPIから取得

--- static/memories/app.js
+++ static/memories/app.js
@@ -1,10 +1,33 @@
 $(document).ready(function() {
 
+  // TODO レスポンシブ
+  // TODO 再検索時結果クリア
+  // TODO 検索待ちgifアニメ
+  // TODO 本文検索
+  // TODO 3アイドル検索（ドロップダウンを3つ)
+  // TODO 外部リンクは別タブで
+
   var speaker1 = "";
-  $("a.speaker1").click(function(){
-    speaker1 = $(this).text();
+  // アイドル一覧を取得してドロップダウン化
+  $.ajax({
+    type: "GET",
+    url: "http://million-memories-server.herokuapp.com/idols",
+    dataType: "json",
+    success: function(data){
+      var idols = $.map(data, function(idol) {
+        return $("<li></li>", {role: "presentation"})
+          .append($("<a></a>", {role: "menuitem", tabindex: "-1", class: "speaker1", href: "#", text: idol.name}));
+      });
+    // ドロップダウン追加
+    $("#speaker1").append(idols);
+    // ドロップダウン選択時
+    $("a.speaker1").click(function(){
+       speaker1 = $(this).text();
+    });
+    }
   });
 
+  // 検索
   $("#search").click(function(){
     $dialogs = $('#dialogs');
     $.ajax({
